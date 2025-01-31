55f47684a0d1220ddaecaafa1920caaa558b6624
labstraction <labstraction@protonmail.com>
2025-01-31 10:39:42 +0100

    merging

--- utils.lua
+++ utils.lua
@@ -39,4 +39,33 @@ utils.log = function(value, depth, key)
   end
 end
 
+utils.uuid = function()
+  local fn = function(x)
+      local r = math.random(16) - 1
+      r = (x == "x") and (r + 1) or (r % 4) + 9
+      return ("0123456789abcdef"):sub(r, r)
+  end
+  return (("xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx"):gsub("[xy]", fn))
+end
+
+utils.random = function(min, max)
+  local min, max = min or 0, max or 1
+  return (min > max and (love.math.random()*(min - max) + max)) or (love.math.random()*(max - min) + min)
+end
+
+utils.pushRotate = function(x, y, r)
+  love.graphics.push()
+  love.graphics.translate(x, y)
+  love.graphics.rotate(r or 0)
+  love.graphics.translate(-x, -y)
+end
+
+utils.pushRotateScale = function(x, y, r, sx, sy)
+  love.graphics.push()
+  love.graphics.translate(x, y)
+  love.graphics.rotate(r or 0)
+  love.graphics.scale(sx or 1, sy or sx or 1)
+  love.graphics.translate(-x, -y)
+end
+
 return utils
