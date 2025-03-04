9ba8379f4be05bffad933f15e849807df71f3063
labstraction <45163263+labstraction@users.noreply.github.com>
2025-03-04 12:15:55 +0000

    comment

--- main_old.lua
+++ main_old.lua
@@ -1,56 +1,56 @@
-local ship = require("ecs.entities.ship")
-local enemy = require("ecs.entities.enemy")
-local camera = require ("ecs.systems.cameraSystem")
-local physicsSystem = require("ecs.systems.physicsSystem")
-local renderSystem = require("ecs.systems.renderSystem")
-local starfieldSystem = require("ecs.systems.starfieldSystem")
-local utils = require("libs.utils")
-local systems
-local player
-local world
+-- local ship = require("ecs.entities.ship")
+-- local enemy = require("ecs.entities.enemy")
+-- local camera = require ("ecs.systems.cameraSystem")
+-- local physicsSystem = require("ecs.systems.physicsSystem")
+-- local renderSystem = require("ecs.systems.renderSystem")
+-- local starfieldSystem = require("ecs.systems.starfieldSystem")
+-- local utils = require("libs.utils")
+-- local systems
+-- local player
+-- local world
 
 
-function love.load()
+-- function love.load()
 
-  world = love.physics.newWorld(0, 0, true)
-  love.physics.setMeter(16)
+--   world = love.physics.newWorld(0, 0, true)
+--   love.physics.setMeter(16)
 
-  systems = {
-    starfieldSystem:new(),
-    physicsSystem:new(world),
-    renderSystem:new(),
-  }
+--   systems = {
+--     starfieldSystem:new(),
+--     physicsSystem:new(world),
+--     renderSystem:new(),
+--   }
 
-  camera = camera:new()
-  camera:setScale(0.5)
+--   camera = camera:new()
+--   camera:setScale(0.5)
 
-  player = ship:new(world, 400, 300, 1)
+--   player = ship:new(world, 400, 300, 1)
 
-  for i = 1, 10 do
-    local x = utils.random(0, 2000)
-    local y = utils.random(0, 2000)
-    enemy:new(world, x, y, 3)
-  end
-end
+--   for i = 1, 10 do
+--     local x = utils.random(0, 2000)
+--     local y = utils.random(0, 2000)
+--     enemy:new(world, x, y, 3)
+--   end
+-- end
 
-function love.update(dt)
-  world:update(dt)
-  camera:setTarget(player)
-  camera:update(dt)
-  systems[1]:update(dt, player)
-  systems[2]:update(dt, world, player)  
+-- function love.update(dt)
+--   world:update(dt)
+--   camera:setTarget(player)
+--   camera:update(dt)
+--   systems[1]:update(dt, player)
+--   systems[2]:update(dt, world, player)  
 
-end
+-- end
 
-function love.draw()
+-- function love.draw()
 
-  camera:apply()
-  for _, system in ipairs(systems) do
-    if system.draw then
-      system:draw(world)
-    end
-  end
-  camera:clear()
+--   camera:apply()
+--   for _, system in ipairs(systems) do
+--     if system.draw then
+--       system:draw(world)
+--     end
+--   end
+--   camera:clear()
   
-end
+-- end
 
